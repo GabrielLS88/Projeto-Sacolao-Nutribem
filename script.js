ca76e27eaa791e320293bd865b7faa443649c032
Gabriel <lopes.biel2588@gmail.com>
2023-11-10 12:15:24 -0300

    Ultimas alterações

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ document.addEventListener('DOMContentLoaded', function() {
         overlay.style.display = 'none'; // Esconde o overlay
     }) ;
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const openButtonProdutos = document.getElementById('btn-produtos');
+    const popUpProdutos = document.getElementById('pop-produtos');
+    const closeButtonProdutos = document.getElementById('close-produtos');
+    const overlay = document.getElementById('overlay');
+    
+    openButtonProdutos.addEventListener('click', function() {
+        popUpProdutos.style.display = 'block'; // Mostra o pop
+        overlay.style.display = 'block'; // Exibe o overlay
+    });
+    
+    closeButtonProdutos.addEventListener('click', function() {
+        popUpProdutos.style.display = 'none'; // Esconde o pop
+        overlay.style.display = 'none'; // Esconde o overlay
+    }) ;
+});
